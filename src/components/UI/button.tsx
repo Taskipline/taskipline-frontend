a77b7dea41b18e0f95f1147ba1025c1a6a400b73
Ayomide Akintan <emzyakints2005@gmail.com>
2025-07-01 10:04:49 +0100

    test formatter

--- src/components/UI/button.tsx
+++ src/components/UI/button.tsx
@@ -10,7 +10,7 @@ export default function Button({
   return (
     <button
       onClick={onClick}
-      className={`px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition ${className}`}
+      className={`px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-600 transition ${className}`}
     >
       {children}
     </button>

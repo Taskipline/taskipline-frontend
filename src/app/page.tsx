1a082edb0ec16425cf67c6b45ed3e3583c27bf4a
Ayomide Akintan <emzyakints2005@gmail.com>
2025-07-01 11:46:38 +0100

    fixed build error [fonts]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Button from '@/components/UI/button'
 import Image from 'next/image'
 
 export default function Home() {
@@ -13,7 +12,6 @@ export default function Home() {
           height={38}
           priority
         />
-        <Button onClick={() => console.log('Button clicked!')}>Hello</Button>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{' '}

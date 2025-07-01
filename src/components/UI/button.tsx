054871948f042ebee98ac110b68e6f95db6c8e7e
Ayomide Akintan <emzyakints2005@gmail.com>
2025-07-01 09:58:33 +0100

    test

---
+++ src/components/UI/button.tsx
@@ -0,0 +1,18 @@
+export default function Button({
+  children,
+  //   onClick,
+  className = '',
+}: {
+  children: React.ReactNode
+  //   onClick: () => void;
+  className?: string
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className={`px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition ${className}`}
+    >
+      {children}
+    </button>
+  )
+}
